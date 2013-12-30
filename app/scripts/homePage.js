ae8d87ed3a5c63929ff0c5ac381bccd1482ba96c
Charlie Martin <cmmartin24@gmail.com>
2013-12-30 14:32:44 -0500

    inital commit

---
+++ app/scripts/homePage.js
@@ -0,0 +1,216 @@
+angular.module('app.homePage', ['ngResource', 'ngAnimate'])
+
+/*
+ * HomeCtrl controller
+ * it should attach feeds to the scope
+ * it should set the selected entry on ENTRY_SELECTED event
+ * it should set the selected entry to null on ENTRY_DESELECTED event
+ */
+.controller('HomeCtrl', function($scope, Feeds) {
+
+	$scope.selectedEntry = null;
+
+  Feeds.get().then(function(feeds) {
+  	$scope.feeds = feeds;
+  }, function(error) {
+  	console.log(error);
+  });
+
+  $scope.$on('ENTRY_SELECTED', function(event, entry) {
+  	$scope.selectedEntry = entry;
+  });
+
+  $scope.$on('ENTRY_DESELECTED', function() {
+  	$scope.selectedEntry = null;
+  });
+})
+
+/*
+ * FeedSources service
+ * it should return an array of absolute urls
+ */
+.factory('FeedSources', function () {
+	return [
+    {url: 'http://feeds.feedburner.com/TechCrunch/'},
+    {url: 'http://feeds.arstechnica.com/arstechnica/index'},
+    {url: 'http://feeds.feedburner.com/GoogleEarthBlog'},
+    {url: 'http://feeds.gawker.com/gizmodo/full'}
+  ];
+})
+
+/*
+ * FeedLoader service
+ * it should send JSONP requests to Google Feed API
+ */
+.factory('FeedLoader', function ($resource) {
+	return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
+		fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+	});
+})
+
+/*
+ * Feeds service
+ * it should call FeedLoader.fetch on all FeedSources
+ */
+.service('Feeds', function ($rootScope, FeedLoader, FeedSources, $q) {
+
+	this.get = function() {
+		var deferred = $q.defer(), feeds = [], numSources = FeedSources.length, i = 0;
+
+		for (; i < numSources; i++) {
+			FeedLoader.fetch({q: FeedSources[i].url, num: 10}, {}, function(response) {
+				feeds.push(response.responseData.feed);
+				if (feeds.length === numSources) deferred.resolve(feeds);
+			});
+		}
+		return deferred.promise;
+	};
+
+})
+
+/*
+ * feed directive
+ * it should display the feed title
+ * it should display multiple entries
+ */
+.directive('feed', function () {
+	return {
+    restrict: 'E',
+    template: '<div>' +
+          			'<h5 class="feed-title">{{model.title}}</h5>' + 
+    						'<div class="feed">' +
+    							'<div class="entries" ng-repeat="entry in model.entries">' +
+    								'<entry model="entry"></entry>' +
+    							'</div>' +
+    						'</div>' +
+    					'</div>',
+    replace: true,
+    scope: {
+    	model: '='
+    }
+  };
+})
+
+/*
+ * entry directive
+ * it should display the title and content of the entry
+ * it should set the background image
+ * it should not set a background image with an invalid file extension
+ */
+.directive('entry', function ($animate) {
+	return {
+    restrict: 'E',
+    replace: true,
+    template: '<div ng-click="select()" class="entry">' +
+    						'<div class="entry-inner">' +
+      						'<h3 class="entry-title">{{model.title}}</h3>' + 
+      						'<p class="entry-preview">{{model.contentSnippet}}</p>' +
+      						'<p>posted {{model.publishedDate |  timeago}}</p>' +
+      					'</div>' +
+    					'</div>',
+    scope: {
+    	model: '='
+    },
+    link: function(scope, element, attrs) {
+
+    	scope.select = function() {
+	    	scope.$emit('ENTRY_SELECTED', scope.model);
+	    }
+
+    	/* Insanity alert: The below code is only necessary due to the inconsistencies in providing
+			 * image urls in RSS entries. First, we look for a designated image url, if none, we
+			 * pull the first image's src from the entry's content. Finally, we check
+			 * if the src has a valid image file extension to avoid displaying an ad
+			 */
+    	var imgUrl = (scope.model.mediaGroups)
+	      		? scope.model.mediaGroups[0].contents[0].url
+	      		: $(scope.model.content).find('img')[0].src,
+	      	allowedExtensions = ['jpg', 'png', 'gif', 'peg'],
+	      	fileExtension = imgUrl.slice(imgUrl.length - 3, imgUrl.length);
+
+    	if (allowedExtensions.indexOf(fileExtension) > -1) {
+    		$(element).css('background', 'url(' + imgUrl + ') center center');
+    	}
+    }
+  };
+})
+
+/*
+ * modal directive
+ * it should be hidden when there is no selected entry
+ * it should be shown when an entry is selected
+ * it should display the title and content of the entry
+ */
+.directive('modal', function () {
+	return {
+    restrict: 'C',
+    replace: true,
+    template: '<div ng-show="model">' +
+							  '<div class="modal-dialog">' +
+							    '<div class="modal-content">' +
+							      '<div class="modal-header">' +
+							        '<button type="button" class="close" ng-click="deselect()" aria-hidden="true">&times;</button>' +
+							        '<h3 class="modal-title"><a ng-href="{{model.link}}" target="_blank">{{model.title}}</a></h3>' +
+							        '<p><small>{{model.publishedDate | formattedFullDate}}<small></p>' +
+							      '</div>' +
+							      '<div class="modal-body"></div>' +
+							    '</div>' +
+							  '</div>' +
+							'</div>',
+    scope: {     	
+    	model: '='
+    },
+    link: function(scope, element, attrs) {
+
+    	scope.$watch('model', function() {
+    		if (scope.model) {
+    			// render as html
+    			element.find('.modal-body').html(scope.model.content);
+    		}
+    	});
+
+    	scope.deselect = function() {
+    		scope.$emit('ENTRY_DESELECTED');
+    	}
+    }
+  };
+})
+
+.directive('fullscreen', function () {
+	return {
+    restrict: 'A',
+    link: function(scope, element) {
+    	element.bind('click', function() {
+    		var el = document.documentElement,
+       			request = el.requestFullScreen 
+      									|| el.webkitRequestFullScreen 
+      									|| el.mozRequestFullScreen
+      									|| angular.noop; // browser not supported. do nothing
+  			request.call(el);
+    	});
+    }
+  };
+})
+
+/*
+ * formattedFullDate Filter
+ * it should covert a full datetime to human format
+ */
+.filter('formattedFullDate', function() {
+	return function(d) {
+  	return d ? moment(d).format('MMMM Do YYYY, h:mm a') : '';
+	};
+})
+
+/*
+ * timeago Filter
+ * it should convert a date to the amount of time from now
+ */
+.filter('timeago', function() {
+  return function(d) {
+    return d ? moment(d).fromNow() : '';
+  };
+});
+
+
+	
---
+++ test/unit/homePageSpec.js
@@ -0,0 +1,282 @@
+/*
+ * HomeCtrl controller
+ * it should attach feeds to the scope
+ * it should set the selected entry on ENTRY_SELECTED event
+ * it should set the selected entry to null on ENTRY_DESELECTED event
+ */
+describe('Controller: HomeCtrl', function() {
+
+  var scope, 
+      $httpBackend, 
+      ctrl, 
+      sampleEntry = {sample: 'entry'};
+
+  var MockFeedsService = function() {
+    this.get = function() {
+      return {
+        then: function(callback) {
+          return callback(sampleEntry); // simulate promise resolution
+        }
+      };
+    }
+  }
+
+  beforeEach(function(){
+    module('app.homePage', function ($provide) {
+      $provide.value('Feeds', new MockFeedsService());
+    });
+    inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('HomeCtrl', {$scope: scope});
+    });
+
+  });
+
+  it('should attach feeds to the scope', function() {
+    expect(scope.feeds).toBe(sampleEntry);
+  });
+
+  it('should set the selected entry on ENTRY_SELECTED event', function() {
+    scope.selectedEntry = null;
+    scope.$broadcast('ENTRY_SELECTED', sampleEntry);
+    expect(scope.selectedEntry).toBe(sampleEntry);
+  });
+
+  it('should set the selected entry to null on ENTRY_DESELECTED event', function() {
+    scope.selectedEntry = sampleEntry;
+    scope.$broadcast('ENTRY_DESELECTED');
+    expect(scope.selectedEntry).toBe(null);
+  });
+
+});
+
+/*
+ * FeedSources service
+ * it should return an array of absolute urls
+ */
+describe('Service: FeedSources', function() {
+  beforeEach(function () {
+    module('app.homePage');
+  });
+
+   it('should return an array of absolute urls', inject(function (FeedSources) {
+    expect(FeedSources[0].url).toContain('http');
+  }));
+});
+
+/*
+ * FeedLoader service
+ * it should send JSONP requests to Google Feed API
+ */
+describe('Service: FeedLoader', function() {
+
+  var feedUrl = 'TEST',
+      requestUrl = 'http://ajax.googleapis.com/ajax/services/feed/load?callback=JSON_CALLBACK&num=10&q=' + feedUrl + '&v=1.0',
+      mockResponse = {'responseData': {'feed': 'sample feed'}};
+
+  beforeEach(module('app.homePage'));
+
+  it('should send JSONP requests to Google Feed API', inject(function(FeedLoader, $httpBackend) {
+
+    $httpBackend.expectJSONP(requestUrl).respond(200, mockResponse);
+
+    FeedLoader.fetch({q: feedUrl, num: 10}, {}, function(response) {
+      expect(response.responseData.feed).toBe(mockResponse.responseData.feed);
+    });
+
+    $httpBackend.flush();
+  }));
+});
+
+/*
+ * Feeds service
+ * it should call FeedLoader.fetch on all FeedSources and return the result promise
+ */
+describe('Service: Feeds', function() {
+
+  var MockFeedLoaderService = function() {
+    this.fetch = function(irrelevant, irrelevant, callback) {
+      callback({'responseData': {'feed': 'sample feed'}});
+    }
+  };
+
+  beforeEach(function() {
+    module('app.homePage', function ($provide) {
+      $provide.value('FeedLoader', new MockFeedLoaderService());
+    });
+  });
+
+  it('should call FeedLoader.fetch on all FeedSources and return the result promise', inject(function(Feeds, FeedSources) {
+
+    Feeds.get().then(function(response) {
+      expect(response.length).toBe(FeedSources.length);
+    });
+
+  }));
+});
+
+/*
+ * feed directive
+ * it should display the feed title
+ * it should display multiple entries
+ */
+describe('Directive: feed', function() {
+  var element, scope;
+
+  beforeEach(module('app.homePage'));
+
+  beforeEach(inject(function($compile, $rootScope){
+    scope = $rootScope.$new();
+    scope.feed = {
+      title: 'title', 
+      entries:[
+      {
+        mediaGroups: [
+          {
+            contents: [
+              {url: 'http://sample.com/picture.png'}
+            ]
+          }
+        ]
+      },
+      { 
+        mediaGroups: [
+          {
+            contents: [
+              {url: 'http://sample.com/picture.png'}
+            ]
+          }
+        ]
+      }
+    ]};
+    element = $compile('<feed model="feed"></feed>')(scope);
+    scope.$digest();
+  }));
+
+  it('should display the feed title', function() {
+    expect(element.find('.feed-title').html()).toBe('title');
+  });
+
+  it('should display multiple entries', function() {
+    expect(element.find('.entries').children.length).toEqual(2);
+  });
+});
+
+/*
+ * entry directive
+ * it should display the title and content of the entry
+ * it should set the background image
+ * it should not set a background image with an invalid file extension
+ */
+describe('Directive: entry', function() {
+  var element, scope;
+
+  var mockEntry = {
+    title: 'Sample',
+    contentSnippet: 'preview',
+    publishedDate: 'Sat, 28 Dec 2013 14:38:11 -0800',
+    mediaGroups: [
+      {
+        contents: [
+          {url: 'http://sample.com/picture.png'}
+        ]
+      }
+    ]
+  };
+
+  beforeEach(module('app.homePage'));
+
+  beforeEach(inject(function($compile, $rootScope){
+    scope = $rootScope.$new();
+    scope.entry = mockEntry;
+    element = $compile('<entry model="entry"></entry>')(scope);
+    scope.$digest();
+  }));
+
+  it('should display the title and content of the entry', function() {
+    expect(element.find('.entry-title').html()).toBe('Sample');
+    expect(element.find('.entry-preview').html()).toBe('preview');
+  });
+
+  it('should set the background image', function() {
+    expect(element.css('background')).toContain('http://sample.com/picture.png');
+  });
+
+  it('should not set a background image with an invalid file extension', function() {
+    scope.entry.mediaGroups[0].contents[0].url = 'http://sample.com/notanimage';
+    scope.$digest();
+    expect(element.css('background')).not.toContain('http://sample.com/notanimage');
+  });
+
+});
+
+/*
+ * modal directive
+ * it should be hidden
+ * it should be shown when an entry is selected
+ * it should display the title and content of the entry
+ */
+describe('Directive: modal', function() {
+  var element, scope;
+
+  beforeEach(module('app.homePage'));
+
+  beforeEach(inject(function($compile, $rootScope){
+    scope = $rootScope.$new();
+    element = $compile('<div class="modal" model="selectedEntry"></div>')(scope);
+    scope.$digest();
+  }));
+  
+  it('should be hidden when there is no selected entry', function() {
+    expect(element.hasClass('ng-hide')).toBe(true);
+  });
+
+  it('should be shown when an entry is selected', function() {
+    scope.selectedEntry = 'anything truthy';
+    scope.$digest();
+    expect(element.hasClass('ng-hide')).not.toBe(true);
+  });
+
+  it('should display the title and content of the entry', function() {
+    scope.selectedEntry = {title: 'title', content: 'some content'};
+    scope.$digest();
+    expect(element.find('div.modal-header').html()).toContain('title');
+    expect(element.find('.modal-body').html()).toBe('some content');
+  });
+});
+
+/*
+ * formattedFullDate Filter
+ * it should covert a full datetime to human format
+ */
+describe('Filter: formattedFullDate', function () {
+  var filter;
+  beforeEach(function () {
+    module('app.homePage');
+    inject(function ($filter) {
+      filter = $filter('formattedFullDate');
+    });
+  });
+
+  it('should covert a full datetime to human format', function () {
+    expect(filter('Sat, 28 Dec 2013 14:38:11 -0800')).toBe('December 28th 2013, 5:38 pm');
+  });
+});
+
+/*
+ * timeago Filter
+ * it should convert a date to the amount of time from now
+ */
+describe('Filter: timeago', function () {
+  var filter;
+  beforeEach(function () {
+    module('app.homePage');
+    inject(function ($filter) {
+      filter = $filter('timeago');
+    });
+  });
+
+  it('should convert a date to the amount of time from now', function () {
+    expect(filter('Sat, 28 Dec 2013 14:38:11 -0800')).toContain('ago');
+  });
+});
